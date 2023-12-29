542d1023fa1a879eacae6f7829f2124ff59d3440
aumizy <aumizy01@gmail.com>
2023-12-28 21:24:47 -0800

    strtok

---
+++ _strtok.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+
+
+/**
+ * _strtok - Splits a string into tokens using specified delimiters.
+ *
+ * @input: The string to be tokenized
+ * @delim: The delimiter
+ * Return: A reference to the next token or NULL if no tokens remain.
+ */
+
+
+
+char *_strtok(char *input, const char *delim)
+{
+    static char *tracker;
+    char *token;
+    if (input != NULL)
+        tracker = input;
+    while ((tracker != NULL) && (_strchr(delim, *tracker) != NULL) &&
+           (*tracker != '\0'))
+        tracker++;
+    token = tracker;
+    if (tracker == NULL || *tracker == '\0')
+        return (NULL);
+    while (*tracker != '\0')
+    {
+        if (_strchr(delim, *tracker) != NULL)
+        {
+            *tracker = '\0';
+            tracker++;
+            return (token);
+        }
+        tracker++;
+    }
+
+    if (*tracker == '\0')
+    {
+        tracker = NULL;
+        return (token);
+    }
+    return (NULL);
+}
