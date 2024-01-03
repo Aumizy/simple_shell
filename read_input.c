54b468175d7ea8142bda499da9a1b5b118deebab
aumizy <aumizy01@gmail.com>
2024-01-02 22:52:42 -0800

    read

---
+++ read_input.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+
+
+/**
+ * read_input - A function for reading user input.
+ * @eof_stats: The memory address of a var that stores the end-of-file (EOF) stats
+ * Return: (input) - a string that contains the input
+ */
+
+
+
+char *read_input(int *eof_stats)
+{
+    char *input = NULL;
+    size_t input_len;
+    int bytes_read;
+
+    input_len = 0;
+    *eof_stats = 0;
+    bytes_read = getline(&input, &input_len, stdin);
+    if (bytes_read == -1)
+    {
+        if (feof(stdin))
+        {
+            write(1, "\n", 1);
+            *eof_stats = 1;
+        }
+        else
+        {
+            perror("_getline");
+            free(input);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (input[bytes_read - 1] == '\n')
+        input[bytes_read - 1] = '\0';
+
+    return (input);
+}
